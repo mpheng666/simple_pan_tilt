80f7f513405428ff9862ed3b597413035f582bb0
mpheng666 <mpheng96@gmail.com>
2020-09-27 20:17:14 +0800

    create_simple_pan_tilt

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/lycoris/mech_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lycoris/mech_ws/devel;/opt/ros/melodic'.split(';') if '/home/lycoris/mech_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/lycoris/mech_ws/devel;/home/lycoris/ws_moveit/devel;/opt/ros/melodic'.split(';') if '/home/lycoris/mech_ws/devel;/home/lycoris/ws_moveit/devel;/opt/ros/melodic' != '' else []
